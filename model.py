26ff07d6a6a3b1af497092ed3aad3ef0af9f22ce
MobtgZhang <33253392+MobTgZhang@users.noreply.github.com>
2020-04-30 14:15:24 +0800

    Add files via upload

---
+++ model.py
@@ -0,0 +1,58 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+class Similarity(nn.Module):
+    def __init__(self,mem_dim,hidden_dim,num_classes):
+        super(Similarity,self).__init__()
+        self.mem_dim = mem_dim
+        self.hidden_dim = hidden_dim
+        self.num_classes = num_classes
+        self.wh = nn.Linear(2*mem_dim,hidden_dim)
+        self.wp = nn.Linear(hidden_dim,num_classes)
+    def forward(self,lvec,rvec):
+        mult_dist = torch.mul(lvec,rvec)
+        abs_dist = torch.abs(torch.add(lvec,-rvec))
+        vec_dist = torch.cat((mult_dist,abs_dist),dim=2)
+        out = torch.sigmoid(self.wh(vec_dist.sum(dim=1)))
+        out = F.softmax(self.wp(out),dim=1)
+        return out
+class RNNSentiment(nn.Module):
+    def __init__(self,vocab_size,embedding_dim,mem_dim,hid_dim,num_layers,rnn_type,dropout,bidirectional,num_classes):
+        super(RNNSentiment,self).__init__()
+        self.vocab_size = vocab_size
+        self.embedding_dim = embedding_dim
+        self.num_layers = num_layers
+        self.rnn_type = rnn_type
+        self.dropout = dropout
+        self.bidirectional = bidirectional
+        self.num_classes = num_classes
+
+        # word embedding_dim
+        self.word_emb = nn.Embedding(vocab_size,embedding_dim)
+        # RNN layers
+        if rnn_type == "RNN":
+            self.sent_rnn = nn.RNN(input_size=embedding_dim,hidden_size=mem_dim,num_layers=num_layers,
+                                   batch_first=True,dropout=dropout,bidirectional=bidirectional)
+        elif rnn_type == "GRU":
+            self.sent_rnn = nn.GRU(input_size=embedding_dim, hidden_size=mem_dim, num_layers=num_layers,
+                                   batch_first=True, dropout=dropout, bidirectional=bidirectional)
+        elif rnn_type == "LSTM":
+            self.sent_rnn = nn.LSTM(input_size=embedding_dim, hidden_size=mem_dim, num_layers=num_layers,
+                                   batch_first=True, dropout=dropout, bidirectional=bidirectional)
+            # (seq_len, batch, num_directions * hidden_size)
+        else:
+            raise TypeError("Unknown RNN model type:%s"%str(rnn_type))
+        hidden_size = mem_dim * 2 if bidirectional else mem_dim * 1
+        self.similarity = Similarity(hidden_size,hid_dim,num_classes)
+    def forward(self,sentA,sentB):
+        '''
+        :param sentA:(batch,seq_len)
+        :param sentB:(batch,seq_len)
+        :return:
+        '''
+        embA = self.word_emb(sentA)
+        embB = self.word_emb(sentB)
+        hidA,_ = self.sent_rnn(embA)
+        hidB,_ = self.sent_rnn(embB)
+        out = self.similarity(hidA,hidB)
+        return out
